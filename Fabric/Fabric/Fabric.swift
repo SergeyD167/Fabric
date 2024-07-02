4c1b77ffb4bb9d7c2eda39e013011102a4a43df7
Sergey Dyatlov <sergeyd020501@gmail.com>
2024-07-02 17:37:24 +0300

    lesson6

--- Fabric/Fabric/Fabric.swift
+++ Fabric/Fabric/Fabric.swift
@@ -7,3 +7,32 @@
 
 import Foundation
 
+class Fabric {
+    
+    var employees: [Person] = []
+    
+    func hireEmployee(firstName: String, lastName: String, age: Int, series: String, number: String, dateOfIssue: Date) -> Person {
+        let newEmployee = Person(firstName: firstName, lastName: lastName, age: age, passport: nil)
+        let employeePassport = Passport(series: series, number: number, dateOfIssue: dateOfIssue, person: newEmployee)
+        newEmployee.passport = employeePassport
+        employees.append(newEmployee)
+        print("\(firstName) \(lastName) hired")
+        return newEmployee
+    }
+    
+    func fireEmployee(employee: Person) {
+        if let index = employees.firstIndex(where: { $0 === employee }) {
+            employees.remove(at: index)
+            print("\(employee.firstName) \(employee.lastName) fired")
+        }
+    }
+    
+    func produceMaterial(employee: Person, material: String, quantity: Int) {
+        if employees.contains(where: { $0 === employee }) {
+            employee.producedMaterials[material, default: 0] += quantity
+            print("\(employee.firstName) \(employee.lastName) produced \(quantity) \(material)")
+        } else {
+            print("Employee \(employee.firstName) \(employee.lastName) not found in the fabric")
+        }
+    }
+}
--- Fabric/Fabric/Passport.swift
+++ Fabric/Fabric/Passport.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class Passport {
+    
+    let series: String
+    let number: String
+    let dateOfIssue: Date
+    weak var person: Person?
+    
+    init(series: String, number: String, dateOfIssue: Date, person: Person?) {
+        self.series = series
+        self.number = number
+        self.dateOfIssue = dateOfIssue
+        self.person = person
+        print("Passport \(series) \(number) init")
+    }
+    
+    deinit {
+        print("Passport \(series) \(number) deinit")
+    }
+}
