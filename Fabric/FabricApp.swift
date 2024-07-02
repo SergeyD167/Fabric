110acaa6f3e33c293780336d95bf02b36aed5c8d
Sergey Dyatlov <sergeyd020501@gmail.com>
2024-07-02 10:41:51 +0300

    Initial Commit

---
+++ Fabric/FabricApp.swift
@@ -0,0 +1,17 @@
+//
+//  FabricApp.swift
+//  Fabric
+//
+//  Created by Сергей Дятлов on 02.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FabricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
